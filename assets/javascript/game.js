5307605f9573dbab3e5ae534beaf2d29a67719e4
Tyler Winters <maxdavin1984@gmail.com>
2019-09-03 15:18:38 -0700

    updated minor items, started JS coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,29 @@
+
+
 //TODO: generate random number between 19-120 for game object
+
+var targetScore = Math.floor(Math.random()*120)+1
+var randomNumber = ""
+$('#random').text(targetScore)
+
 //TODO: each crystal needs a value between 1-12 assigned randomly
+
+
+
 //TODO: clicking gems adds specific gem value to player score
 //TODO: if player score matches game object, player wins, increasing "win" score
 //TODO: if player score exceeds game object, player loses, increasing "lose" score
 //TODO: On win or lose, both game object and crystal values randomly reset
+//TODO: - win or lose, display win or lose message above win/lose score
+
+
+
+
+//random target score
+//display score to user
+//generate random value to each crystal and attach
+//when user clicks on crystal (click handler event), add current value to user score
+//update user score
+//check if game over
+//reset
+
