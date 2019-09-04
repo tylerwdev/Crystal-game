5753fe2728975c00ccd8877711a68445f825f67a
Tyler Winters <maxdavin1984@gmail.com>
2019-09-04 09:32:28 -0700

    minor change to html, finished JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,33 @@ var playerScore = document.getElementById("playerScore");
 var playerWin = document.getElementById("playerWin");
 var playerLose = document.getElementById("playerLose")
 
+var reset = function () {
+    targetScore = Math.floor(Math.random()*101)+19
+    $('#random').text(targetScore);
+
+    dataCrystal1 = Math.floor(Math.random()*12)+1
+    redCrystalDiv = $('#red-crystal')
+    redCrystalDiv.attr('crystal-value', dataCrystal1);
+
+    dataCrystal2 = Math.floor(Math.random()*12)+1
+    blueCrystalDiv = $('#blue-crystal')
+    blueCrystalDiv.attr('crystal-value', dataCrystal2);
+
+    dataCrystal3 = Math.floor(Math.random()*12)+1
+    greenCrystalDiv = $('#green-crystal')
+    greenCrystalDiv.attr('crystal-value', dataCrystal3);
+
+    dataCrystal4 = Math.floor(Math.random()*12)+1
+    yellowCrystalDiv = $('#yellow-crystal')
+    yellowCrystalDiv.attr('crystal-value', dataCrystal4);
+
+    score = 0
+    playerScore.textContent = " " + score
+    playerScore = document.getElementById("playerScore");
+    
+    console.log(score)
+}
+
 // generate random number between 19-120 for game object
 
 var targetScore = Math.floor(Math.random()*101)+19
@@ -39,10 +66,13 @@ $('#red-crystal').on('click', function() {
     if (score===targetScore){
         wins++
         document.getElementById('flash-message').innerHTML = 'You Won!'
+        reset()
     }
+    
     if (score > targetScore) {
         losses++
         document.getElementById('flash-message').innerHTML = 'You lost!'
+        reset()
     }
 
     playerWin.textContent = "" + wins
@@ -57,10 +87,12 @@ $('#blue-crystal').on('click', function() {
     if (score===targetScore){
         wins++
         document.getElementById('flash-message').innerHTML = 'You Won!'
+        reset()
     }
     if (score > targetScore) {
         losses++
         document.getElementById('flash-message').innerHTML = 'You lost!'
+        reset()
     }
 
     playerWin.textContent = "" + wins
@@ -75,10 +107,12 @@ $('#green-crystal').on('click', function() {
     if (score===targetScore){
         wins++
         document.getElementById('flash-message').innerHTML = 'You Won!'
+        reset()
     }
     if (score > targetScore) {
         losses++
         document.getElementById('flash-message').innerHTML = 'You lost!'
+        reset()
     }
 
     playerWin.textContent = "" + wins
@@ -93,32 +127,19 @@ $('#yellow-crystal').on('click', function() {
     if (score===targetScore){
         wins++
         document.getElementById('flash-message').innerHTML = 'You Won!'
+        reset()
     }
     if (score > targetScore) {
         losses++
         document.getElementById('flash-message').innerHTML = 'You lost!'
+        reset()
     }
 
     playerWin.textContent = "" + wins
     playerLose.textContent = "" + losses
 })
 
-//TODO: if player score matches game object, player wins, increasing "win" score, reset game object & crystal values
-
-
-
-//TODO: if player score exceeds game object, player loses, increasing "lose" score, reset game object & crystal values
-
-//TODO: - win or lose, display win or lose message above win/lose score
-
-
-
-
-//random target score
-//display score to user
-//generate random value to each crystal and attach
-//when user clicks on crystal (click handler event), add current value to user score
-//update user score
-//check if game over
-//reset
-
+// if player score matches game object, player wins, increasing "win" score, reset game object & crystal values
+// if player score exceeds game object, player loses, increasing "lose" score, reset game object & crystal values
+// win or lose, display win or lose message above win/lose score
+//-above statements are achieved in each crystal function and reset function
