eca718d71796095b5dc974a23a26283be29f9452
Tyler Winters <maxdavin1984@gmail.com>
2019-09-03 18:50:04 -0700

    random value for game, each crystal has random value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,30 @@
 
 //TODO: generate random number between 19-120 for game object
 
-var targetScore = Math.floor(Math.random()*120)+1
-var randomNumber = ""
+var targetScore = Math.floor(Math.random()*101)+19
 $('#random').text(targetScore)
+
+//committed to here at school
 
 //TODO: each crystal needs a value between 1-12 assigned randomly
 
+var dataCrystal1 = Math.floor(Math.random()*12)+1
+var redCrystalDiv = $('#red-crystal')
+redCrystalDiv.attr('crystal-value', dataCrystal1)
 
+var dataCrystal2 = Math.floor(Math.random()*12)+1
+var blueCrystalDiv = $('#blue-crystal')
+blueCrystalDiv.attr('crystal-value', dataCrystal2)
+
+var dataCrystal3 = Math.floor(Math.random()*12)+1
+var greenCrystalDiv = $('#green-crystal')
+greenCrystalDiv.attr('crystal-value', dataCrystal3)
+
+var dataCrystal4 = Math.floor(Math.random()*12)+1
+var yellowCrystalDiv = $('#yellow-crystal')
+yellowCrystalDiv.attr('crystal-value', dataCrystal4)
+
+//all 4 crystals havent been committed yet
 
 //TODO: clicking gems adds specific gem value to player score
 //TODO: if player score matches game object, player wins, increasing "win" score
