ea93fc9947ebcd4b259a0af2d9ca41f71801bcf2
Tyler Winters <maxdavin1984@gmail.com>
2019-09-03 19:44:10 -0700

    each crystal when clicked puts specific into total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,13 @@
+var score = []
+var wins = 0
+var losses = 0
 
+var playerScore = document.getElementById("playerScore");
 
 //TODO: generate random number between 19-120 for game object
 
 var targetScore = Math.floor(Math.random()*101)+19
 $('#random').text(targetScore)
-
-//committed to here at school
 
 //TODO: each crystal needs a value between 1-12 assigned randomly
 
@@ -25,9 +27,33 @@ var dataCrystal4 = Math.floor(Math.random()*12)+1
 var yellowCrystalDiv = $('#yellow-crystal')
 yellowCrystalDiv.attr('crystal-value', dataCrystal4)
 
-//all 4 crystals havent been committed yet
-
 //TODO: clicking gems adds specific gem value to player score
+
+
+$('#red-crystal').on('click', function() {
+    score.push(dataCrystal1);
+
+    playerScore.textContent = " " + score
+})
+
+$('#blue-crystal').on('click', function() {
+    score.push(dataCrystal2);
+
+    playerScore.textContent = " " + score
+})
+
+$('#green-crystal').on('click', function() {
+    score.push(dataCrystal3);
+
+    playerScore.textContent = " " + score
+})
+
+$('#yellow-crystal').on('click', function() {
+    score.push(dataCrystal4);
+
+    playerScore.textContent = " " + score
+})
+
 //TODO: if player score matches game object, player wins, increasing "win" score
 //TODO: if player score exceeds game object, player loses, increasing "lose" score
 //TODO: On win or lose, both game object and crystal values randomly reset
